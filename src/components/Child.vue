2945f02c3709d1b4d31242bb3a5558a6f052dd15
tomokazuUchiyama <wisteria-0004@m3.gyao.ne.jp>
2019-02-06 23:04:47 +0900

    test

---
+++ src/components/Child.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+  <p>
+    For a guide and recipes on how to configure / customize this project,<br>
+    check out the
+    <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  </p>
+  <div>{{abc}}</div>
+  </section>
+</template>
+<script>
+import axios from "axios";
+import mock from "fetch-mock";
+mock.once("https://api.doorkeeper.jp/", {
+  eee: 333
+});
+export default {
+  name: "Child",
+  data: function() {
+    return {
+      abc: ""
+    };
+  },
+  created: function() {
+    axios
+      .get("https://api.doorkeeper.jp/")
+      .then(response => (this.abc = response));
+  }
+};
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
